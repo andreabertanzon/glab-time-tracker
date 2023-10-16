587566a5137536413890eb398441ab3ac9291e67
andreabertanzon <andrea.bertanzon@protonmail.com>
2023-10-16 20:31:25 +0200

    small seconds mistake on timeFormatter

--- src/helpers/timeFormatter.ts
+++ src/helpers/timeFormatter.ts
@@ -2,7 +2,7 @@ function formatTime(inputSeconds: number): string {
   const hours = Math.floor(inputSeconds / 3600)
   const minutes = Math.floor((inputSeconds % 3600) / 60)
   const seconds = Math.floor((inputSeconds % 3600) % 60)
-  const secondsString = seconds < 10 ? `0${inputSeconds}` : inputSeconds
+  const secondsString = seconds < 10 ? `0${seconds}` : seconds
   const minutesString = minutes < 10 ? `0${minutes}` : minutes
   return `${hours}:${minutesString}:${secondsString}`
 }
